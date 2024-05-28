3348f0d3753fb1479c506d5c98d3028b231729e1
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-05-28 15:01:04 -0500

    Categorias.js actualizado

--- src/app/components/Categorias.js
+++ src/app/components/Categorias.js
@@ -1,9 +1,25 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+
 
 const Categorias = ({ seleccionarCategoria }) => {
     const [categorias, setCategorias] = useState([]);
+    
     useEffect(()=>{
-        axios.get
-    })
-}
+        fetch('https://fakestoreapi.com/products/categories')
+        .then(response=>response.json())
+        .then(data => setCategorias(data))
+        .catch(error => console.error('Error al obtener las categorías:', error));
+    },[]);
+
+    return(
+        <div>
+            <h2>Categorias</h2>
+            <ul>
+                {categorias.map((categoria)=>(
+                    <li key={categoria} onClick={() => seleccionarCategoria(categoria)}>{categoria}</li>
+                ))}   
+            </ul>
+        </div>
+    );
+};
+export default Categorias;
