3598559b9192db70ed0370556a1ae36641588427
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-05-28 18:51:56 -0500

    ARREGLADOO

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import Categorias from './components/Categorias';
 import Productos from './components/Productos';
 import DetalleProducto from './components/DetalleProducto';
 import Carrito from './components/Carrito';
+import pageCss from './page.module.css';
 
 const App = () => {
     const [categoria, setCategoria] = useState(null); // Corrección aquí
     const [idProductoSeleccionado, setIdProductoSeleccionado] = useState(null);
-    const [elementosCarrito, setElementosCarrito] = useState(
-        JSON.parse(sessionStorage.getItem('elementosCarrito')) || []
-    );
+    // const [elementosCarrito, setElementosCarrito] = useState(
+    //     JSON.parse(sessionStorage.getItem('elementosCarrito')) || []
+    // );
+
+    const [elementosCarrito, setElementosCarrito] = useState([]);
 
     const agregarAlCarrito = (producto) => {
         const nuevosElementosCarrito = [...elementosCarrito, producto];
@@ -17,9 +22,18 @@ const App = () => {
         sessionStorage.setItem('elementosCarrito', JSON.stringify(nuevosElementosCarrito));
     };
 
+    useEffect(()=>{
+        if(elementosCarrito.length==0){
+            setElementosCarrito( JSON.parse(sessionStorage.getItem('elementosCarrito')) || [])
+        }
+    },[]);
+
+
+    
+
     return (
         <div>
-            <h1>Tienda</h1>
+            <h1 className={pageCss.description}>Tienda</h1>
             <Categorias seleccionarCategoria={setCategoria} />
             <Productos categoria={categoria} seleccionarProducto={setIdProductoSeleccionado} /> {/* Corrección aquí */}
             <DetalleProducto idProducto={idProductoSeleccionado} agregarAlCarrito={agregarAlCarrito} />
