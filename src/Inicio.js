77749d80256577d3ffb54f36e7c2901cb72eb7a0
JuanFeMartinez <jfmartinezv@uniboyaca.edu.co>
2024-05-28 17:14:24 -0500

    se quito app por que estaba generando conflicto

---
+++ src/Inicio.js
@@ -0,0 +1,10 @@
+import Categorias from "./components/Categorias";
+
+export default function Inicio () {
+    const categorias = [];
+    return(
+        <section>
+            <h1>tiendita lol</h1>
+        </section>
+    )
+}
